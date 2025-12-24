119f529f737d8a1824b421de841ca7ce01534d05
Carey <carey.stevens@ablex.healthcare>
2025-12-24 20:19:39 +1300

    implement

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,24 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+ 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request: Request) {
+    const requestUrl = new URL(request.url);
+	let firebaseProjectUrl = "https://ablex-production.firebaseapp.com";
+    if (requestUrl.pathname.startsWith("/__/auth/")) {
+		let firebaseProjectUrl = "https://ablex-production.firebaseapp.com";
+		if(requestUrl.hostname.startsWith("ablex-development")) {
+			firebaseProjectUrl = "https://ablex-development.firebaseapp.com";
+		} else if(requestUrl.hostname.startsWith("ablex-staging")) {
+			firebaseProjectUrl = "https://ablex-staging.firebaseapp.com";
+		} 
+      const transformedUrl = new URL(
+        firebaseProjectUrl + requestUrl.pathname + requestUrl.search,
+      );
+      const newRequest = new Request(transformedUrl.toString(), request);
+      return fetch(newRequest);
+    }
+    return new Response("Not found", { status: 404 });
+  },
+};
